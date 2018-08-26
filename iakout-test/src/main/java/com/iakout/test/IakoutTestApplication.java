9c91e14581deedc63891317806b86b23b9d89c48
iakout <iakout@iakout.com>
2018-08-26 23:41:06 +0800

    2018/8/26

---
+++ iakout-test/src/main/java/com/iakout/test/IakoutTestApplication.java
@@ -0,0 +1,12 @@
+package com.iakout.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IakoutTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IakoutTestApplication.class, args);
+	}
+}
